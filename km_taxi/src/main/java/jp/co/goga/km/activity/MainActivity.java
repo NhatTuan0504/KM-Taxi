79d04e58addbb0e9d9d6ae2f3b76981ca7988003
aTruongQuocKhanh <khanhtq@evolableasia.vn>
2016-03-22 10:19:00 +0700

    KM_TAXI_imp. init code

---
+++ km_taxi/src/main/java/jp/co/goga/km/activity/MainActivity.java
@@ -0,0 +1,32 @@
+package jp.co.goga.km.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import jp.co.goga.km.R;
+
+/**
+ * Created by khanhtq on 3/22/16.
+ */
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
